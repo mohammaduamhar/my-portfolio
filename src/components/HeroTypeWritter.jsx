68070b047daa2ab9f1376210d47e62baac1c6def
mohammaduamhar <mamamhar254@gmail.com>
2025-08-28 01:46:39 +0530

    checking git configurations

--- src/components/HeroTypeWritter.jsx
+++ src/components/HeroTypeWritter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HeroTypeWritter = () => {
-  return <div>HeroTypeWritter</div>;
+  return <div>HeroTypeWritter upadte</div>;
 };
 
 export default HeroTypeWritter;
